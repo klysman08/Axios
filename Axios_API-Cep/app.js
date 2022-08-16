d06afb55e913785f1e54eec57e22ee1f1bce3d88
Klysman08 <klysman.rk@gmail.com>
2022-08-16 17:54:05 -0300

    Initial commit

---
+++ Axios_API-Cep/app.js
@@ -0,0 +1,73 @@
+var submitButton = document.querySelector("#app form button");
+var zipCodeField = document.querySelector("#app form input");
+var content = document.querySelector("#app main");
+
+submitButton.addEventListener("click", run);
+
+function run(e) {
+  e.preventDefault();
+
+  var zipCode = zipCodeField.value;
+
+  zipCode = zipCode.replace(" ", "");
+  zipCode = zipCode.replace("-", "");
+  zipCode = zipCode.replace(".", "");
+  zipCode = zipCode.trim();
+
+  /*     console.log(zipCode); */
+  /*  */
+  axios
+    .get("https://viacep.com.br/ws/" + zipCode + "/json/")
+    .then(function (response) {
+      /*  console.log(response) */
+      content.innerHTML = "";
+      createLine(response.data.logradouro);
+      createLine(response.data.bairro);
+      createLine(response.data.localidade);
+      endereco = response.data.logradouro;
+      /* console.log(endereco) */
+    })
+    .catch(function (error) {
+      console.log(error);
+      content.innerHTML = "";
+      createLine("CEP não encontrado");
+    });
+}
+
+console.log(endereco);
+
+function createLine(text) {
+  var line = document.createElement("p");
+  var text = document.createTextNode(text);
+  line.appendChild(text);
+  content.appendChild(line);
+}
+
+var geocoder;
+var map;
+var address = "UFMG";
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: { lat: -34.397, lng: 150.644 },
+  });
+  geocoder = new google.maps.Geocoder();
+  codeAddress(geocoder, map);
+}
+
+function codeAddress(geocoder, map) {
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status === "OK") {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location,
+      });
+    } else {
+      console.log(
+        "Geocode was not successful for the following reason: " + status
+      );
+    }
+  });
+}
